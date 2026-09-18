c6545480d0645e48d69cc39f5483122052c5108f
thiagotgl <195029755+thiagotgl@users.noreply.github.com>
2026-09-18 13:57:39 -0300

    Update ligacoes.ts

--- data/ligacoes.ts
+++ data/ligacoes.ts
@@ -14,6 +14,7 @@ export const ligacoesPorMes = [
 
   { ano: 2026, mes: "JANEIRO", recebidas: 11931, atendidas: 10606, naoAtendidas: 1325 },
   { ano: 2026, mes: "FEVEREIRO", recebidas: 8802, atendidas: 8275, naoAtendidas: 527 },
+  { ano: 2026, mes: "MARÇO", recebidas: 10099, atendidas: 9146, naoAtendidas: 953 },
 
 { ano: 2026, mes: "AGOSTO", recebidas: 8426, atendidas: 8168, naoAtendidas: 258 },
   
